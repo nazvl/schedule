627fa22e1d4d451cd3001de88e588b3d73437301
Vladimir <lismiroslav@yandex.ru>
2025-10-22 16:18:52 +0300

    Vite Plugin PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,39 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {fileURLToPath} from "node:url";
-import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url"
+import tailwindcss from "@tailwindcss/vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+    plugins: [
+        vue(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Schedule App',
+                short_name: 'Schedule',
+                description: 'PWA',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: [
-            { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },],
+            { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+        ],
     },
 })
