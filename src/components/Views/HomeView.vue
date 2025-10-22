b9d2ca78b73f1129c1f0178e0e2235f855b0b879
Vladimir <lismiroslav@yandex.ru>
2025-10-22 14:37:09 +0300

    HomeView

--- src/components/Views/HomeView.vue
+++ src/components/Views/HomeView.vue
@@ -10,16 +10,22 @@ function handleChooseMode(mode: string) {
 </script>
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center p-6">
-    <h1 class="text-center text-3xl md:text-4xl font-bold text-indigo-700 dark:text-blue-300 mb-8 max-w-2xl">Добро пожаловать в электронное расписание!</h1>
+    <h1 class="text-center text-3xl md:text-4xl font-bold text-indigo-700 dark:text-blue-300 mb-8 max-w-2xl">Добро
+      пожаловать в электронное расписание!</h1>
 
     <div class="bg-white dark:bg-gray-800 shadow-lg rounded-xl p-8 max-w-md w-full text-center flex flex-col">
       <p class="mb-6 text-gray-700 dark:text-gray-200">Выберите режим просмотра расписания:</p>
-      <button class="bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 px-6 rounded-lg shadow transition duration-200 mb-4" @click="handleChooseMode('student')">
-        Студент
-      </button>
-      <button class="bg-green-500 hover:bg-green-600 text-white font-medium py-3 px-6 rounded-lg shadow transition duration-200">
-        Преподаватель
-      </button>
+      <div class="inline-flex rounded-md shadow-xs w-full" role="group">
+        <button type="button"
+                class="w-1/2 px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-green-500 hover:text-black focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-blue-500 dark:focus:text-white">
+          Студент
+        </button>
+        <button type="button"
+                class="w-1/2 px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-blue-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-500 dark:focus:text-white">
+          Преподаватель
+        </button>
+      </div>
+
     </div>
   </div>
 </template>
